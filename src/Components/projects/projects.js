ce697814a1d9599c26189c445767f36350432f1b
jakubmat2001 <jakubmatusik11@gmail.com>
2023-12-09 13:49:03 +0000

    adjusted project image slideshow to fit all images

--- src/Components/projects/projects.js
+++ src/Components/projects/projects.js
@@ -18,7 +18,7 @@ const Projects = React.forwardRef((props, ref) => {
             <div className="project-panel-container">
                 <MDBCarousel showControls interval={8000}>
                     <MDBCarouselItem itemId={1} interval={8000}>
-                        <img src={faceRecogn} className='d-block w-100' alt='...' />
+                        <img src={faceRecogn} id="project-images-fc" className='d-block w-100' alt='...' />
                         <div className="image-text">
                             <h5>N-Shine FaceRecogntion - Face Detection App </h5>
                             <p> Live hosted face recognition website with Signin/Register and <b>email verification </b> functionalites.
@@ -31,7 +31,7 @@ const Projects = React.forwardRef((props, ref) => {
                         </div>
                     </MDBCarouselItem>
                     <MDBCarouselItem itemId={2}>
-                        <img src='https://mdbootstrap.com/img/new/slides/042.jpg' className='d-block w-100' alt='...' />
+                        <img src='https://mdbootstrap.com/img/new/slides/042.jpg' id="project-images-2" className='d-block w-100' alt='...' />
                         <div className="image-text">
                             <p>This porject is about blahanal, reinventing something natural, even tho this doesnt owrk  it kinda does as
                                 its wrapping my works as I asked it to do, so thats great
@@ -39,7 +39,7 @@ const Projects = React.forwardRef((props, ref) => {
                         </div>
                     </MDBCarouselItem>
                     <MDBCarouselItem itemId={3}>
-                        <img src={dockKeep} id="dockeep-image" className='d-block w-100' alt='...' />
+                        <img src={dockKeep} id="project-images-dk" className='d-block w-100' alt='...' />
                         <div className="image-text">
                             <h5>DocKeep - Forex Trade documentation App</h5>
                             <p>Installable and deployable forex trade documentation app. Allowing forex a.k.a currency traders to file their trades
