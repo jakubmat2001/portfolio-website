c5c1d178fd925cfcc309c00380862cc506538fdb
jakubmat2001 <jakubmatusik11@gmail.com>
2023-12-22 09:27:04 +0000

    added temp links to project images

--- src/Components/projects/projects.js
+++ src/Components/projects/projects.js
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { MDBCarousel, MDBCarouselItem } from 'mdb-react-ui-kit';
 import faceRecogn from "../../Images/FaceRecognitionProject.png"
 import QuoteCalculator from "../../Images/QuoteCalculator.png"
 import dockKeep from "../../Images/DK.png"
-
 import "./projects.css"
 
 const Projects = React.forwardRef((props, ref) => {
+    const [imageLinks, setImageLinks] = useState({
+        dockKeep: "",
+        quoteCalculator: "",
+        faceRecogn: ""
+    })
+
+    useEffect(() => {
+        setImageLinks({
+            dockKeep: "https://github.com/jakubmat2001/DocKeep",
+            quoteCalculator: "https://github.com/jakubmat2001/web-systems-coursework",
+            faceRecogn: "https://github.com/jakubmat2001/Ai-FaceRecognition-Frontend"
+        })
+    }, [])
+
     return (
         <div ref={ref} className="project-container">
             <div className="project-title-container">
@@ -17,7 +30,8 @@ const Projects = React.forwardRef((props, ref) => {
             <div className="project-panel-container">
                 <MDBCarousel showControls interval={8000}>
                     <MDBCarouselItem itemId={1}>
-                        <img src={faceRecogn} id="project-images-fc" className='d-block w-100' alt='...' />
+                        <a href={imageLinks.faceRecogn} target="_blank" rel="noreferrer">
+                            <img src={faceRecogn} id="project-images-fd" className='d-block w-100' alt='face-detection webapp snipet' /></a>
                         <div className="image-text">
                             <h5>Face Detection Web-App - (Hosted Live) - (2023)</h5>
                             <p> Live hosted face recognition website with Signin/Register and <b>email verification </b> functionalites.
@@ -30,16 +44,18 @@ const Projects = React.forwardRef((props, ref) => {
                         </div>
                     </MDBCarouselItem>
                     <MDBCarouselItem itemId={2}>
-                        <img src={QuoteCalculator} id="project-images-2" className='d-block w-100' alt='...' />
+                        <a href={imageLinks.quoteCalculator} target="_blank" rel="noreferrer">
+                            <img src={QuoteCalculator} id="project-images-qc" className='d-block w-100' alt='quote calculatoer website snipet' /></a>
                         <div className="image-text">
                             <h5>Quote Calculator Website - (Dockerized) - (2023)</h5>
-                            <p> Website utilizing <b>SSR</b> for better searchability, with <b>MongoDB</b> database for managing data & user sessions. Content on a 
-                            website can be accessed without a user sign-in, however they won't be able to add/modify/delete their quotes for any given project untill loged into their own account. Each user can create only one quote per project entering inputs such as: hours, expariance, human-resources to calculate final budget. Random fudgefactor is added in and multiplied by budget to prevent reverse engerning the real budget by another project bidder.
+                            <p> Website utilizing <b>SSR</b> for better searchability, with <b>MongoDB</b> database for managing data & user sessions. Content on a
+                                website can be accessed without a user sign-in, however they won't be able to add/modify/delete their quotes for any given project untill loged into their own account. Each user can create only one quote per project entering inputs such as: hours, expariance, human-resources to calculate final budget. Random fudgefactor is added in and multiplied by budget to prevent reverse engerning the real budget by another project bidder.
                             </p>
                         </div>
                     </MDBCarouselItem>
                     <MDBCarouselItem itemId={3}>
-                        <img src={dockKeep} id="project-images-dk" className='d-block w-100' alt='...' />
+                        <a href={imageLinks.dockKeep} target="_blank" rel="noreferrer">
+                            <img src={dockKeep} id="project-images-dk" className='d-block w-100' alt='docKeep software snipet' /></a>
                         <div className="image-text">
                             <h5>Forex Trade Documentation App - Deployable Software - (2023)</h5>
                             <p>Installable and deployable forex trade documentation app. Allowing forex a.k.a currency traders to file their trades
@@ -49,7 +65,7 @@ const Projects = React.forwardRef((props, ref) => {
                     </MDBCarouselItem>
                 </MDBCarousel>
             </div>
-           
+
         </div>
     )
 })
